65f162a437eeb8cdc5d95e181928c009a5099e12
Juan Techera <juanjose@iovlabs.org>
2021-05-23 18:52:34 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <script src="https://cdn.scaleflex.it/plugins/js-cloudimage-360-view/2.6.0/js-cloudimage-360-view.min.js"></script>
+      <div
+   class="cloudimage-360"
+   data-folder="https://elasticbeanstalk-us-east-2-182336593385.s3.us-east-2.amazonaws.com/Cherry+Blossom/"
+   data-filename="Cherry+Blossom_{index}.jpg"
+   data-amount="10"
+   data-index-zero-base="5"
+></div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
